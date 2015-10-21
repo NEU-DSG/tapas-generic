cfdc3ab393625148f23ec3e0a7f59f5273e26e81
Eli Zoller <zoller.eli@gmail.com>
2015-10-21 09:53:30 -0400

    starting to make fixes for fitting tapasG into the larger tapas interface

--- js/tapas-generic.js
+++ js/tapas-generic.js
@@ -1,22 +1,22 @@
 //This JS made possible by the great work done in teibp/js/teibp.js
 function clearPageBreaks(){
-	$(".tapas-generic pb").css("display","none");
-	$(".tapas-generic .-teibp-pb").css("display","none");
+	jQuery(".tapas-generic pb").css("display","none");
+	jQuery(".tapas-generic .-teibp-pb").css("display","none");
 }
 
 function addPageBreaks(){
     if (Tapas.currentTheme == 'diplomatic') {
-    	$(".tapas-generic pb").css("display","block");
-    	$(".tapas-generic .-teibp-pb").css("display","block");
+    	jQuery(".tapas-generic pb").css("display","block");
+    	jQuery(".tapas-generic .-teibp-pb").css("display","block");
     } else {
-    	$(".tapas-generic pb").css("display","inline");
-    	$(".tapas-generic .-teibp-pb").css("display","inline");
+    	jQuery(".tapas-generic pb").css("display","inline");
+    	jQuery(".tapas-generic .-teibp-pb").css("display","inline");
     }
 }
 
 function init(){
-	$('#pbToggle').click( function(){
-		if($(this).is(':checked')){
+	jQuery('#pbToggle').click( function(){
+		if(jQuery(this).is(':checked')){
 			clearPageBreaks();
 			Tapas.showPbs = false;
 		}else{
@@ -25,16 +25,16 @@ function init(){
 		}
 	});
 	addPageBreaks();
-	$(this).checked = false;
+	jQuery(this).checked = false;
 }
 
-$(document).ready(function(){
+jQuery(document).ready(function(){
 	init();
 });
 
 function switchThemes(event) {
-	$(".tapas-generic").removeClass('diplomatic').removeClass('normal').addClass($(event.target).val());
-	Tapas.currentTheme = $(event.target).val();
+	jQuery(".tapas-generic").removeClass('diplomatic').removeClass('normal').addClass(jQuery(event.target).val());
+	Tapas.currentTheme = jQuery(event.target).val();
 }
 
 function showFacs(num, url, id) {
@@ -42,35 +42,35 @@ function showFacs(num, url, id) {
 	facsWindow.document.write("<html>")
 	facsWindow.document.write("<head>")
 	facsWindow.document.write("<title>TEI Boilerplate Facsimile Viewer</title>")
-	facsWindow.document.write($('#maincss')[0].outerHTML)
-	facsWindow.document.write($('#customcss')[0].outerHTML)
+	facsWindow.document.write(jQuery('#maincss')[0].outerHTML)
+	facsWindow.document.write(jQuery('#customcss')[0].outerHTML)
 	facsWindow.document.write("<link rel='stylesheet' href='../js/jquery-ui/themes/base/jquery.ui.all.css'>")
-	facsWindow.document.write($('style')[0].outerHTML)
+	facsWindow.document.write(jQuery('style')[0].outerHTML)
 	facsWindow.document.write("<script type='text/javascript' src='../js/jquery/jquery.min.js'></script>")
 	facsWindow.document.write("<script type='text/javascript' src='../js/jquery-ui/ui/jquery-ui.js'></script>")
 	facsWindow.document.write("<script type='text/javascript' src='../js/jquery/plugins/jquery.scrollTo-1.4.3.1-min.js'></script>")
 	facsWindow.document.write("<script type='text/javascript' src='../js/teibp.js'></script>")
 	facsWindow.document.write("<script type='text/javascript'>")
-	facsWindow.document.write("$(document).ready(function() {")
-	facsWindow.document.write("$('.facsImage').scrollTo($('#" + id + "'))")
+	facsWindow.document.write("jQuery(document).ready(function() {")
+	facsWindow.document.write("jQuery('.facsImage').scrollTo(jQuery('#" + id + "'))")
 	facsWindow.document.write("})")
 	facsWindow.document.write("</script>")
-	facsWindow.document.write("<script>	$(function() {$( '#resizable' ).resizable();});</script>")
+	facsWindow.document.write("<script>	jQuery(function() {jQuery( '#resizable' ).resizable();});</script>")
 	facsWindow.document.write("</head>")
 	facsWindow.document.write("<body>")
-	facsWindow.document.write($("teiHeader")[0].outerHTML)
-	//facsWindow.document.write("<teiHeader>" + $("teiHeader")[0].html() + "</teiHeader>")
-	//facsWindow.document.write($('<teiHeader>').append($('teiHeader').clone()).html();)
+	facsWindow.document.write(jQuery("teiHeader")[0].outerHTML)
+	//facsWindow.document.write("<teiHeader>" + jQuery("teiHeader")[0].html() + "</teiHeader>")
+	//facsWindow.document.write(jQuery('<teiHeader>').append(jQuery('teiHeader').clone()).html();)
 
-	//facsWindow.document.write($("teiHeader")[0].outerHTML)
+	//facsWindow.document.write(jQuery("teiHeader")[0].outerHTML)
 	facsWindow.document.write("<div id='resizable'>")
 	facsWindow.document.write("<div class='facsImage'>")
-	$(".-teibp-thumbnail").each(function() {
-		facsWindow.document.write("<img id='" + $(this).parent().parent().parent().attr('id') + "' src='" + $(this).attr('src') + "' alt='facsimile page image'/>")
+	jQuery(".-teibp-thumbnail").each(function() {
+		facsWindow.document.write("<img id='" + jQuery(this).parent().parent().parent().attr('id') + "' src='" + jQuery(this).attr('src') + "' alt='facsimile page image'/>")
 	})
 	facsWindow.document.write("</div>")
 	facsWindow.document.write("</div>")
-	facsWindow.document.write($("footer")[0].outerHTML)
+	facsWindow.document.write(jQuery("footer")[0].outerHTML)
 
 	facsWindow.document.write("</body>")
 	facsWindow.document.write("</html>")
