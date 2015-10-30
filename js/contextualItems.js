a85c8e1b7c22c48fe76eed346d09d3f46ba0e144
Eli Zoller <zoller.eli@gmail.com>
2015-10-30 07:06:01 -0400

    trying out alternate positioning on dialog box

--- js/contextualItems.js
+++ js/contextualItems.js
@@ -62,7 +62,7 @@ Tapas.refreshDialog = function(html, target, coords) {
     console.log("tapas ref dialog html is "+html);
     //placing the dialog for data display in the big white space currently there. Adjust position via jQueryUI rules for different behavior
     // jQuery("#tapas-ref-dialog").dialog( "option", "position", { my: "right top+"+coords[0]/2, at: "right top+"+coords[0]/2, of: window });
-    jQuery("#tapas-ref-dialog").dialog( "option", "position", { my: "right top", at: "right top+", of: window });
+    jQuery("#tapas-ref-dialog").dialog( "option", "position", { my: "left top+200", at: "left top+200", of: window });
     var identifierEl = target.children('p.identifier');
     if(identifierEl.length == 0) {
         //dialog title by mouseovered text, where the mouseovered element is passed as target, usually a note
