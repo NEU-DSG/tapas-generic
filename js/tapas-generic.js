86e9fd860cd64cc893ac087d45c3628115d7ef30
Eli Zoller <zoller.eli@gmail.com>
2015-10-30 12:49:00 -0400

    Adding resizable

--- js/tapas-generic.js
+++ js/tapas-generic.js
@@ -40,8 +40,9 @@ function switchTapasThemes(event) {
 
 function showFacs(num, url, id) {
 	console.log("showing facs for num:"+num+" , url:"+url+", id:"+id);
-	jQuery("body").append('<div class="modal fade" id="modal_'+id+'"><div class="modal-dialog"><div class="modal-content"><div class="modal-header"><button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button></div><div class="modal-body"><img src="'+url+'" class="img-responsive"/></div></div><!-- /.modal-content --></div><!-- /.modal-dialog --></div><!-- /.modal -->');
+	jQuery("body").append('<div class="modal fade" id="modal_'+id+'"><div class="modal-dialog"><div class="modal-content"><div class="modal-header"><button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button></div><div class="modal-body"><img src="'+url+'" id="resizable_'+id+'"/></div></div><!-- /.modal-content --></div><!-- /.modal-dialog --></div><!-- /.modal -->');
 	jQuery("#modal_"+id).modal('show');
+	jQuery("#resizable_"+id).resizable();
 	// facsWindow = window.open ("about:blank")
 	// facsWindow.document.write("<html>")
 	// facsWindow.document.write("<head>")
