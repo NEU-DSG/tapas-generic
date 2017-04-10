96f77d2ccabd78ecfba7cd63abb3c34f5834c2c2
Ashley M. Clark <amclark42@gmail.com>
2017-04-10 15:47:04 -0400

    Increase relative heights of headings
    Increase margin between 'ography entries. Attempt to differentiate
    nested 'ographies from their ancestor(s).

--- css/makecss.bash
+++ css/makecss.bash
@@ -1,5 +1,5 @@
 #!/bin/bash          
-lessc tapasG.less > tapasG.css
+#lessc tapasG.less > tapasG.css
 lessc tapasGdiplo.less > tapasGdiplo.css
 lessc tapasGnormal.less > tapasGnormal.css
 #lessc sleepy.less > sleepy.css
