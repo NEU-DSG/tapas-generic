76e0c9d34ff47529e5cece255def5e026c814b86
Ashley M. Clark <amclark42@gmail.com>
2017-11-14 15:36:49 -0500

    Fix facsimile event
    While the JS function Tapas.showFacs() can now be found, it does
    not work using the full-HTML debugging method. I suspect that
    there is an unstated dependence on the Bootstrap JS library.

--- js/tapas-generic.js
+++ js/tapas-generic.js
@@ -38,14 +38,6 @@ $(document).ready(function() {
     $(".tapas-generic #viewBox").change(switchTapasThemes);
   }
 
-  function showFacs(num, url, id) {
-    console.log("showing facs for num:"+num+" , url:"+url+", id:"+id);
-    $(".tapas-generic").append(
-      '<div class="modal fade" id="modal_'+id+'"><div class="modal-dialog"><div class="modal-content"><div class="modal-header"><button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button></div><div class="modal-body"><img src="'+url+'" id="resizable_'+id+'" class="img-resizable ui-widget-content"/></div></div><!-- /.modal-content --></div><!-- /.modal-dialog --></div><!-- /.modal -->');
-    $("#modal_"+id).modal('show');
-    $("#resizable_"+id).resizable({minWidth: 150});
-  }
-
   function switchTapasThemes(event) {
     var newTheme = $(event.target).val();
     $(".tapas-generic").removeClass('diplomatic').removeClass('normal').addClass(newTheme);
